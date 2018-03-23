f387f9d732ac30830b7d8826b8728d99b57fd81e
savigdordscience <36956296+savigdordscience@users.noreply.github.com>
2018-03-23 10:44:41 +0300

    Final Version w/o comments

--- best.R
+++ best.R
@@ -1,5 +1,6 @@
 best <- function(state, outcome)
 {
+  options(warn=-1)
   ## Read outcome data
   outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
@@ -7,7 +8,6 @@ best <- function(state, outcome)
   validState = (state %in% unique(outcomeData[,7]))
   if (!validState)
     stop("invalid state")
-  print("Great State!!!")
 
   ## Return hospital name in that state with lowest 30-day death
   ## rate
@@ -27,12 +27,13 @@ best <- function(state, outcome)
   write.csv(recordsByState,"as3-rcbs-numeric.csv")
   ##recordsByStateNonNA <- recordsByState[,!(is.na(numericColumn))]
   ##write.csv(recordsByStateNonNA,"as3-rcbs-non-na.csv")
-  recordsByStateSorted <- recordsByState[with(recordsByState,order(recordsByState["Hospital.Name"])),]
+  recordsByStateSorted <- recordsByState[with(recordsByState,order(recordsByState[columnName],recordsByState["Hospital.Name"])),]
   write.csv(recordsByStateSorted,"as3-rcbs-sorted.csv")
-  lowest30DaysMortality = min(recordsByStateSorted[,columnName], na.rm = TRUE)
-  print(paste("lowest30DaysMortality=",lowest30DaysMortality))
-  lowestHospitalOutcomes = match(lowest30DaysMortality,recordsByStateSorted[,columnName])
-  print(paste("lowestHospitalOutcomes=",lowestHospitalOutcomes))
-  print(paste("lowestHospitalOutcomes[0]=",lowestHospitalOutcomes))
-  recordsByStateSorted[lowestHospitalOutcomes,"Hospital.Name"]
+  ##lowest30DaysMortality = min(recordsByStateSorted[,columnName], na.rm = TRUE)
+  ##print(paste("lowest30DaysMortality=",lowest30DaysMortality))
+  ##lowestHospitalOutcomes = match(lowest30DaysMortality,recordsByStateSorted[,columnName])
+  ##print(paste("lowestHospitalOutcomes=",lowestHospitalOutcomes))
+  ##print(paste("lowestHospitalOutcomes[0]=",lowestHospitalOutcomes))
+  ##recordsByStateSorted[lowestHospitalOutcomes,"Hospital.Name"]
+  recordsByStateSorted[1,"Hospital.Name"]
 }
